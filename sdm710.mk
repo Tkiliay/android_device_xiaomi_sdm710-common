097c4e5844a3b9548ef83356c356e4261597de0a
Mathieu Chartier <>
2022-11-20 13:19:25 +0530

    sdm710-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -371,6 +371,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsensorndkbridge
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
