e67c670c475c33c7aad282eea7b9ada386ba338a
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-11-29 16:43:55 +0100

    sdm710-common: Link libdlbdsservice against v33 libstagefright_foundation
     * fixes crashes and absurdly high CPU usage while using dolby
    
    [B--B: move libstagefright_foundation-v33 in vendor repo in preparation
    to BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES removal]
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: Ib72b775dae677f3085d0b0e2afcbb709d8690063
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -23,6 +23,9 @@ source "${HELPER}"
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/libdlbdsservice.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
         system_ext/lib64/lib-imsvideocodec.so)
             "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
             ;;
